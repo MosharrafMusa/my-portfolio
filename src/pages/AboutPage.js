40d996ad8a1b370495efbeacc064cc0c884524d3
Mosharraf Musa <mosharrafny@gmail.com>
2021-04-14 15:32:59 -0400

    about page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const AboutPage = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+           <h1>this is about page</h1> 
+        </>
     )
 }
 
