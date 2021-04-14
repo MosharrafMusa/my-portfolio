537275513c7d9a809eda5a527e41ed250e4745b5
Mosharraf Musa <mosharrafny@gmail.com>
2021-04-13 21:56:53 -0400

    topPage css

--- src/components/PageTop/PageTop.js
+++ src/components/PageTop/PageTop.js
@@ -2,9 +2,24 @@ import React from 'react'
 
 const PageTop = () => {
     return (
-        <div>
+        <>
+            <Container fluid={true} className="topFixedPage p-0">
+            <div className= "topPageOverlay">
+                <Container className="topContent">
+                    <Row>
+                        <Col className="text-center">
+                       
+                        <h4 className="topPageSubTitle">About Me</h4>
+                       
+                        </Col>
+                    </Row>
+
+                </Container>
+
+            </div>
             
-        </div>
+        </Container> 
+        </>
     )
 }
 
